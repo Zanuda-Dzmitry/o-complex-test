c93e4ed8c3d73b108365da0b399a370235d4190e
Dzmitry <dzmitryshaviaka@gmail.com>
2025-06-19 20:24:17 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Обязательно для статического экспорта
-  basePath: process.env.GITHUB_PAGES ? "/repo-name" : "", // Замените 'repo-name' на имя вашего репозитория
-  assetPrefix: process.env.GITHUB_PAGES ? "/repo-name/" : "",
+  basePath: process.env.GITHUB_PAGES ? "/o-complex-test" : "", // Замените 'repo-name' на имя вашего репозитория
+  assetPrefix: process.env.GITHUB_PAGES ? "/o-complex-test/" : "",
   trailingSlash: true, // Добавляет `/` к URL (рекомендуется для GitHub Pages)
 };
 
